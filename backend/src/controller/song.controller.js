1351d0733f0d38a3bed28fc5c19ab41cc1b65b89
Abhikappana <abhikappana@gmail.com>
2025-03-07 11:40:27 +0530

    feat: add song route and controller
    - Added new song controller for handling song-related operations
    - Implemented song route to manage song endpoints

---
+++ backend/src/controller/song.controller.js
@@ -0,0 +1,90 @@
+import {
+    Song
+} from '../models/song.model.js';
+
+
+export const getAllSongs = async (req, res) => {
+
+    try {
+        const songs = await Song.find().sort({
+            createdAt: -1
+        });
+        res.json(songs);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getFeatuedSongs = async (req, res) => {
+    try {
+        const songs = await Song.aggregate([{
+                $project: {
+                    id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            },
+
+            {
+                $sample: {
+                    size: 6
+                }
+            }
+        ]);
+        res.json(songs);
+
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getMadeForYou = async (req, res) => {
+    try {
+        const songs = await Song.aggregate([{
+                $project: {
+                    id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            },
+            {
+                $sample: {
+                    size: 4
+                }
+            }
+        ]);
+        res.json(songs);
+
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getTrending = async (req, res) => {
+    try {
+        const songs = await Song.aggregate([{
+                $project: {
+                    id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            },
+            {
+                $sample: {
+                    size: 4
+                }
+            }
+        ]);
+        res.json(songs);
+    } catch (error) {
+        next(error);
+    }
+}
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { getAllSongs,getFeatuedSongs,getMadeForYou,getTrending } from "../controller/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 
-router.get('/',(req,res) => {
-    res.send("Song route with GET method")
-});
+router.get('/',protectRoute,requireAdmin,getAllSongs);
+router.get('/featured',getFeatuedSongs);
+router.get('/madeForYou',getMadeForYou);
+router.get('/trending',getTrending);
+
+
+
+
 
 export default router;
